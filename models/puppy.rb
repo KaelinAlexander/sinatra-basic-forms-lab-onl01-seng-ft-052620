e043dc6e74fdf2e6bf24a32ceeb532cf70b35b9a
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-07-08 16:24:26 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,9 +2,9 @@ class Puppy
 attr_accessor :breed, :name, :age
 
   def initialize(attributes)
-    @name = name:
-    @breed = breed:
-    @age = age:
+    @name = name
+    @breed = breed
+    @age = age
  end
 
 
